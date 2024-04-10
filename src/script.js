eca238fb92a04b4208b86bcab709ae2f38f5e9d8
NtandoRaji <2584925@students.wits.ac.za>
2024-04-10 22:29:50 +0200

    connected to azure api

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
+const baseURL = "https://2584925-lab6-sd-24-api.azurewebsites.net/";
+
 document.addEventListener('DOMContentLoaded', () => {
     const loadCarsBtn = document.getElementById('loadCarsBtn');
     const carList = document.getElementById('carList');
     cars = [];
     loadCarsBtn.addEventListener('click', () => {
-        fetch('http://localhost:3001/cars')
+        fetch(baseURL + 'cars')
             .then(response => response.json())
             .then(data => {
                 cars = data;
@@ -28,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 function addCar(newCar) {
-    fetch('http://localhost:3001/cars', {
+    fetch(baseURL + 'cars', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -60,7 +62,7 @@ carForm.addEventListener('submit', event => {
 // Function to remove a car
 function removeCar(index) {
     const carId = cars[index].id;
-    fetch(`http://localhost:3001/cars/${carId}`, {
+    fetch(baseURL + `cars/${carId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
